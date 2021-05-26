72e9dc8b0a02f5f39e349dc06eea2260c3d4c5b2
Elise <eal109@georgetown.edu>
2021-05-26 10:48:15 -0400

    Update joins to remove unnecessary NAs in env. data

--- code/abundance_analysis.R
+++ code/abundance_analysis.R
@@ -9,28 +9,30 @@ library(ggplot2)
 library(r2glmm)
 library(sjPlot)
 library(MuMIn)
+library(MASS)
 #abundance metrics
 
 abund<-read_csv("data/derived/naba_OWS_abundances.csv") %>%
-  select(cell, year=ObsYear, doy, CountID, SurveyID, ObsMonth, ows.grp=group, abund.bph, log.abund, SR)
+  dplyr::select(cell, year=ObsYear, doy, CountID, SurveyID, ObsMonth, ows.grp=group, abund.bph, log.abund, SR)
 
 #explanatory variables
 env<-read_csv("data/derived/envir.csv") %>%
-  select(cell, year, warmearly, warmlateopen, gr_mn_for, gr_mn_open, gr_mn_lag, spring.dev, summer.dev, FFD.dev, FRD.dev, cell_lat)
+  dplyr::select(cell, year, warmearly, warmlateopen, gr_mn_for, gr_mn_open, gr_mn_lag, spring.dev, summer.dev, FFD.dev, FRD.dev, cell_lat)
 pheno<-read_csv("data/derived/simpleton_pheno_pdfs-OutlierDetection.csv") %>%
-  select(year, cell=HEXcell,ows.grp=code, doy=x, pdf=y) %>%
+  dplyr::select(year, cell=HEXcell,ows.grp=code, doy=x, pdf=y) %>%
   group_by(year, cell, ows.grp) %>% 
+  mutate(maxpdf=max(pdf, na.rm=T)) %>%
   mutate(cumpr=cumsum(pdf)*.5, on=ifelse(cumpr>0.01,1,0),med=ifelse(cumpr>0.5,1,0),term=ifelse(cumpr>0.99,1,0)) %>%
-  mutate(onc=cumsum(on),medc=cumsum(med),termc=cumsum(term)) %>%
-  summarize(onset=doy[onc==1], median=doy[medc==1], duration=doy[termc==1]-doy[onc==1]) %>%
+  mutate(onc=cumsum(on),medc=cumsum(med),termc=cumsum(term),) %>%
+  summarize(onset=doy[onc==1], median=doy[medc==1], duration=doy[termc==1]-doy[onc==1], maxdoy=doy[pdf==maxpdf]) %>%
   group_by(cell, ows.grp) %>% 
   mutate(onset.hm=mean(onset, na.rm=T), duration.hm=mean(duration, na.rm=T), onset.d=onset-onset.hm, dur.d=duration-duration.hm)
-  
+
 
 #previous year abundance
 abund.py<-abund %>%
   mutate(year=year+1) %>%
-  select(cell, year, CountID, ows.grp, abund.py=abund.bph, logab.py=log.abund)
+  dplyr::select(cell, year, CountID, ows.grp, abund.py=abund.bph, logab.py=log.abund)
 
 
 #combine tables
@@ -39,25 +41,85 @@ ab1<-merge(x = ab, y = env, by = intersect(names(ab), names(env)), all.x = TRUE)
 ab.final<-merge(x = ab1, y = abund.py, by.x=c("cell", "year", "CountID", "ows.grp"), by.y=c("cell", "year", "CountID", "ows.grp"), all.x = TRUE)
 
 #abundance Model
-naba.1<-(ab.final) %>% mutate(ows.grp=as.factor(ows.grp),summer.dev1=summer.dev/100,year=year-2002, doy=doy-150, on.dev=onset.d/7, dur.dev=dur.d/7, gr_mn_lag=gr_mn_lag/7, FR.dev=FRD.dev/7)
+naba.1<-(ab.final) %>% mutate(ows.grp=as.factor(ows.grp),summer.dev1=summer.dev/100,year=year-2002, on.dev=onset.d/7, dur.dev=dur.d/7, gr_mn_lag=gr_mn_lag/7, FR.dev=FRD.dev/7, daylag=doy-maxdoy, abslag=abs(doy-maxdoy))
 
 
 #naba.1$ows.grp<-factor(naba.1$ows.grp,levels(factor(naba.1$ows.grp))[c(1,2,3)])
-ab.yr.full<-lmer(log.abund~0+ows.grp+logab.py+warmearly:ows.grp+warmlateopen+on.dev+dur.dev+year:ows.grp+gr_mn_lag+as.factor(ObsMonth)+doy+FR.dev:ows.grp+(1|cell)+(1|CountID), data=naba.1)
+
+ab.yr.full<-lmer(log.abund~0+ows.grp+logab.py+warmearly:ows.grp+warmlateopen+on.dev+abslag+dur.dev+year:ows.grp+gr_mn_lag+as.factor(ObsMonth)+doy+FR.dev:ows.grp+(1|cell)+(1|CountID), data=naba.1)
 r.squaredGLMM(ab.yr.full)     
 summary(ab.yr.full)
 (step_resyr <- step(ab.yr.full))
-finalyr <- get_model(step_resyr)
+finalyr <- get_model(step_resyr) #stepAIC(ab.yr.full)
 anova(finalyr)
 summary(finalyr)
 r.squaredGLMM(finalyr)
+AIC(finalyr)
+
+fixed_only<-lm(log.abund~0+ows.grp+logab.py+on.dev+as.factor(ObsMonth)+abslag+daylag+ows.grp:warmearly+ows.grp:FR.dev, data=naba.1)
+AIC(fixed_only)
+
+
+fixed.full<-lm(log.abund~-1+ows.grp+logab.py+warmearly:ows.grp+warmlateopen+on.dev+dur.dev+year:ows.grp+gr_mn_lag+as.factor(ObsMonth)+doy+FR.dev:ows.grp, data=naba.1)
+step_fix <- stepAIC(fixed.full, direction = "both", 
+                    trace = FALSE)
+summary(step_fix)
+
+
+summary(fixed.full)     
+step_fixyr<-step(fixed.full)
+fixedyr <- stepAIC(step_fixyr)
+anova(fixedyr)
+summary(fixedyr)
+r.squaredGLMM(fixedyr)
+AIC(fixedyr)
+
+
+
+phtest_glmer <- function (glmerMod, glmMod, ...)  {  ## changed function call
+  coef.wi <- coef(glmMod)
+  coef.re <- fixef(glmerMod)  ## changed coef() to fixef() for glmer
+  vcov.wi <- vcov(glmMod)
+  vcov.re <- vcov(glmerMod)
+  names.wi <- names(coef.wi)
+  names.re <- names(coef.re)
+  coef.h <- names.re[names.re %in% names.wi]
+  dbeta <- coef.wi[coef.h] - coef.re[coef.h]
+  df <- length(dbeta)
+  dvcov <- vcov.re[coef.h, coef.h] - vcov.wi[coef.h, coef.h]
+  stat <- abs(t(dbeta) %*% as.matrix(solve(dvcov)) %*% dbeta)  ## added as.matrix()
+  pval <- pchisq(stat, df = df, lower.tail = FALSE)
+  names(stat) <- "chisq"
+  parameter <- df
+  names(parameter) <- "df"
+  alternative <- "choose fixed effects model"
+  res <- list(statistic = stat, p.value = pval, parameter = parameter, 
+              method = "Hausman Test",  alternative = alternative,
+              data.name=deparse(getCall(glmerMod)$data))  ## changed
+  class(res) <- "htest"
+  return(res)
+}
 
 #plots
 plot_model(finalyr, values=T)
 
+phtest_glmer(finalyr, fixedyr)
+plotresult<-plot_model(fixedyr, values=T)
+
+fixedlabels<-c(rep("",15))
+fixedlabels[which(summary(fixedyr)$coefficients[,4]<0.05)]<-"*"
+plotresult + annotate(geom="text", x=rev(c(1:15)), y=rep(-1,15), label=fixedlabels)
+
+summary(naba.1)
+#####
+## Fixed effects model is better
+
 hist(naba.1$year)
 hist(naba.1$CountID)
 
+
+
+#Plots
 ggplot(data=naba.1) + geom_boxplot(aes(factor(ObsMonth),log.abund, fill=ows.grp)) + 
   scale_x_discrete(labels = c('June','July')) + theme_classic()
 
--- code/gather_envir_vars.R
+++ code/gather_envir_vars.R
@@ -27,7 +27,7 @@ cpc.gdd<-na.omit(read_csv("data/envir/cpcGDD_hex_2000-2020.csv")) %>%
   summarize(gdd=sum(meanGDD, na.rm=T)) %>%
   pivot_wider(names_from = season, values_from = gdd) %>%
   group_by(cell) %>% mutate(gdd.comb=spring.gdd+summer.gdd, spring.mean=mean(spring.gdd, na.rm=T),spring.dev=(spring.gdd-spring.mean),
-                                  summer.mean=mean(summer.gdd, na.rm=T),summer.dev=(summer.gdd-summer.mean)) 
+                            summer.mean=mean(summer.gdd, na.rm=T),summer.dev=(summer.gdd-summer.mean)) 
 
 
 ## WINTER CLIMATE
@@ -38,13 +38,14 @@ cpc.gdd<-na.omit(read_csv("data/envir/cpcGDD_hex_2000-2020.csv")) %>%
 cpc.winter<-read.csv("data/envir/cpc_winter.csv" ) %>%
   select(cell=hex_cell, year=season, FRD, FFD) %>%
   group_by(cell) %>%
+  filter(year %in% c(2001:2020)) %>%
   mutate(FFDhm=mean(FFD, na.rm=T),FRDhm=mean(FRD, na.rm=T)) %>%
   group_by(cell, year) %>%
   summarize(FRD=mean(FRD, na.rm=T), FFD=mean(FFD, na.rm=T),FFD.dev=ifelse(is.na(FFD),NA,(FFD-FFDhm)),FRD.dev=ifelse(is.na(FRD),NA,(FRD-FRDhm))) %>%
   select(cell, year, FFD, FRD, FFD.dev, FRD.dev)
 
 
-clim.data<-merge(cpc.gdd, cpc.winter, by=c("cell","year"))
+clim.data<-merge(cpc.gdd, cpc.winter, by=c("cell","year"), all.x=T, all.y=T)
 
 write.csv(clim.data, file="data/envir/cpc.comb.csv")
 ## GREENUP
@@ -59,26 +60,38 @@ green.all<-readRDS("data/envir/MidGreenup-2020-08-06-all.rds") %>%
   select(year, cell, gr_mn_all=gr_mn, gr_pall=gr_pcell, cell_lat, cell_lng)
 
 #Greenup: calculate half-max DOY of open pixels
-greendev<-merge(green.all,green.for, by=intersect(names(green.all),names(green.for))) %>%
+greendev<-merge(green.all,green.for, by=intersect(names(green.all),names(green.for)), all.x=T, all.y=T) %>%
   mutate(p_open=gr_pall-gr_pfor) %>%
   mutate(gr_mn_open=ifelse(p_open>0,((gr_mn_all*gr_pall - gr_mn_for*gr_pfor)/(gr_pall-gr_pfor)),NA), gr_mn_lag=(gr_mn_open-gr_mn_for))
 
-env_var<-merge(greendev, clim.data, by=intersect(names(greendev), names(clim.data)))
+env_var<-merge(greendev, clim.data, by=intersect(names(greendev), names(clim.data)), all.x=T, all.y=T)
 
-ggplot(data=filter(greenup, year>2009), aes(x=greenFZ, y=greenDZ, color=gr_pcell)) + geom_point() + 
-  labs(x="Greenup in forest pixels", y="Lag of all pixels", title="Z-score Half-max Greenup Date", color="percent forest") + 
+ggplot(data=filter(greendev, year>2009), aes(x=gr_mn_for, y=gr_mn_lag, color=gr_pfor)) + geom_point() + 
+  labs(x="Greenup in forest pixels", y="Lag of open pixels", color="percent forest") + 
   theme_minimal()
+
+
+## filter data to what is needed and PCA 
+abund<-read_csv("data/derived/naba_OWS_abundances.csv") %>%
+  dplyr::select(cell, year=ObsYear) %>%
+  summarize(cellyr=paste(cell,year,sep=".")) 
+
+tempabund<-abund %>% dplyr::select(cellyr) %>% group_by(cellyr) %>% tally()
+
+env_var<-env_var %>% mutate(cellyr=paste(cell,year,sep=".")) %>%
+  filter(cellyr %in% tempabund$cellyr)
 
 corr <- round(cor(na.omit(env_var[,c(7,10,11,14,16,18,21:22)])), 1)
 ggcorrplot(corr, method = "circle")
 
-env_var<-na.omit(env_var)
 
 pca_res <- prcomp(na.omit(env_var[,c(7,10,14)]), scale. = TRUE)
 summary(pca_res)
 pca_res
-pca<-data.frame(warmearly=-(pca_res$x[,1]),warmlateopen=pca_res$x[,2] )
-env2<-cbind((env_var),pca)
+pca<-data.frame(cell=env_var$cell[!is.na(env_var$gr_mn_for)], year=env_var$year[!is.na(env_var$gr_mn_for)],  warmearly=-(pca_res$x[,1]),warmlateopen=pca_res$x[,2] )
+
+
+env2<-merge(env_var[,-23],pca, by=intersect(names(env_var),names(pca)),all.x=T)
 corr <- round(cor(env2[,c(23,24,16,18,21,22)]), 1)
 ggcorrplot(corr, method = "circle")
 
